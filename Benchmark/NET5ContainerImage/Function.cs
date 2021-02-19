2c07773e5247447ddf797737bbff7e879f12a08d
Iren SALTALI <iren@saltali.com>
2021-02-20 00:46:36 +0300

    publishing changes

--- Benchmark/NET5ContainerImage/Function.cs
+++ Benchmark/NET5ContainerImage/Function.cs
@@ -1,11 +1,6 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats.Jpeg;
-using SixLabors.ImageSharp.Processing;
-using System.IO;
-using Amazon.S3;
-using System;
+using System.Collections.Generic;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -23,77 +18,22 @@ namespace NET5ContainerImage
         /// <returns></returns>
         public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
         {
-            //var s = new SortedSet<int>();
-            //for (int n = 0; n < 100_000; n++)
-            //{
-            //    s.Add(n);
-            //}
-
-            //for (int i = 0; i < 10_000_000; i++)
-            //{
-            //    var tmpVar = s.Min;
-            //}
-
-            //return new APIGatewayProxyResponse
-            //{
-            //    Body = "Done",
-            //    StatusCode = 200
-            //};
-
-            var bucketName = "net-benchmark-test"; //update the bucket name
-            var objectKey = "d6a15c89-48ae-4bca-9001-d4486fc66bae.png"; //update the object key
-            try
+            var s = new SortedSet<int>();
+            for (int n = 0; n < 100_000; n++)
             {
-                Stream imageStream = new MemoryStream();
-                using (var s3Client = new AmazonS3Client(Amazon.RegionEndpoint.EUCentral1))
-                {
-                    var getObjectResponse = s3Client.GetObjectAsync(new Amazon.S3.Model.GetObjectRequest
-                    {
-                        BucketName = bucketName,
-                        Key = objectKey
-                    });
-
-                    using (Stream responseStream = getObjectResponse.Result.ResponseStream)
-                    {
-                        using (var image = Image.Load(responseStream))
-                        {
-                            // Create B&W thumbnail
-                            image.Mutate(ctx => ctx.Grayscale().Resize(200, 200));
-                            image.Save(imageStream, new JpegEncoder());
-                            imageStream.Seek(0, SeekOrigin.Begin);
-                        }
-                    }
-
-
-                    var thumbnailObjectKey = "thumbnails/" + objectKey;
-
-                    LambdaLogger.Log("Thumbnail file Key: " + thumbnailObjectKey);
-
-                    var putObjectResponse = s3Client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest
-                    {
-                        BucketName = bucketName,
-                        Key = thumbnailObjectKey,
-                        InputStream = imageStream
-                    });
-                }
-
-                return new APIGatewayProxyResponse
-                {
-                    Body = "Done",
-                    StatusCode = 200
-                };
+                s.Add(n);
             }
-            catch (Exception e)
+
+            for (int i = 0; i < 10_000_000; i++)
             {
-                context.Logger.LogLine($"Make sure object and bucket exist and your bucket is in the same region as this function");
-                context.Logger.LogLine(e.Message);
-                context.Logger.LogLine(e.StackTrace);
-                return new APIGatewayProxyResponse
-                {
-                    Body = "Error",
-                    StatusCode = 500
-                };
+                var tmpVar = s.Min;
             }
+
+            return new APIGatewayProxyResponse
+            {
+                Body = "Done",
+                StatusCode = 200
+            };
         }
     }
 }
